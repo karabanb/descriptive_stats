5ef18d1505c48a762f8a5b7269563b8e2403847b
karabanb <bkarabands@gmail.com>
2019-07-16 16:44:09 +0200

    dopisanie funckji 'range'

--- variatioons_measures.R
+++ variatioons_measures.R
@@ -1,2 +1,12 @@
 
 # some initialization 
+
+range <- function(variables) {
+  max(variables) - min(variables)
+}
+
+# Unikamy pokrywania się nazw funkcji !
+
+# jeżeli nazwt funkcji się pokrywają, można to obejść wskazująć, żktóregop pakietu ma brać funckję 
+# poprzez 'nazwa_pakietu::funkcja'
+range(1:4)
